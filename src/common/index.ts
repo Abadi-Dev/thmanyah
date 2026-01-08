ad890fc151569c76d4d12b40b97a64409092ea2e
Abdulelah Alamri <Abadi_dev@outlook.com>
2026-01-08 22:32:49 +0300

    dtos and filters and common lib setup

---
+++ src/common/index.ts
@@ -0,0 +1,8 @@
+// DTOs
+export * from './dto/pagination.dto';
+
+// Interfaces
+export * from './interfaces/paginated-result.interface';
+
+// Filters
+export * from './filters/http-exception.filter';
