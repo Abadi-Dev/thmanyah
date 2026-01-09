ee4156fe0419b8a23aa8c5ccf78273047ad2601e
Abdulelah Alamri <Abadi_dev@outlook.com>
2026-01-09 18:48:54 +0300

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,21 @@ async function bootstrap() {
 
   // we use this exception  for consistent format
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // 1. Create the configuration
+  const config = new DocumentBuilder()
+    .setTitle('Thmanyah Content Discovery API')
+    .setDescription('The API for managing and discovering programs/podcasts')
+    .setVersion('1.0')
+    .addTag('cms', 'Internal management endpoints (not authenticated ATM)')
+    .addTag('discovery', 'public search and filter endpoints')
+    .build();
+
+  // 2. Create the document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 3. Setup the UI endpoint (e.g., http://localhost:3000/docs)
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/programs/dto/create-program.dto.ts
+++ src/programs/dto/create-program.dto.ts
@@ -9,42 +9,52 @@ import {
   MaxLength,
   Matches,
 } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ProgramType } from '../entities/program.entity';
 
 export class CreateProgramDto {
+  @ApiProperty({ example: 'The Thmanyah Show', description: 'Public title' })
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
   title: string;
 
+  @ApiProperty({
+    example: 'the-thmanyah-show',
+    description: 'URL-friendly identifier',
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
-    message: 'slug must be lowercase with hyphens only (e.g., "my-program")',
+    message: 'slug must be lowercase with hyphens only',
   })
   slug: string;
 
+  @ApiPropertyOptional({ example: 'A deep dive into...' })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ enum: ProgramType, example: ProgramType.PODCAST })
   @IsEnum(ProgramType)
   type: ProgramType;
 
+  @ApiPropertyOptional({ example: 'https://random-server.com/img.jpg' })
   @IsOptional()
   @IsUrl()
   thumbnailUrl?: string;
 
-  @IsOptional()
-  @IsUrl()
-  videoUrl?: string;
-
+  @ApiPropertyOptional({
+    example: 'dQw4w9WgXcQ',
+    description: 'YouTube ID if the program is already uploaded',
+  })
   @IsOptional()
   @IsString()
   @MaxLength(50)
   youtubeId?: string;
 
+  @ApiPropertyOptional({ example: 3600, description: 'Duration in seconds' })
   @IsOptional()
   @IsInt()
   @Min(0)
